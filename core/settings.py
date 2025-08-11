868ff559f357f2e251b73726fefba0ed95b687fc
sujan <najus777@gmail.com>
2025-08-11 18:16:36 +0545

    update

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9gpg(zy8f*0z)=^rcv-n6h7((i%xedvxab@iaz$0$7jh6&y2+p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.1.77']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','bishankhu.hitzz.co', '192.168.1.77']
 
 
 # Application definition
--- store/urls.py
+++ store/urls.py
@@ -4,9 +4,9 @@ app_name = 'store'
 
 urlpatterns = [
     path('',views.home, name="home"),
-    path('/Bishankhu-mustard-oil',views.product, name="product"),
-    path('/mustard-oil-service',views.service, name="service"),
-    path('/mustard-oil-about',views.about, name="about"),
-    path('/bishankhu-mustard-oil-contact',views.contact, name="contact"),
-    path('/message',views.message, name="message"),
+    path('Bishankhu-mustard-oil/',views.product, name="product"),
+    path('mustard-oil-service/',views.service, name="service"),
+    path('mustard-oil-about/',views.about, name="about"),
+    path('bishankhu-mustard-oil-contact/',views.contact, name="contact"),
+    path('message/',views.message, name="message"),
 ]
